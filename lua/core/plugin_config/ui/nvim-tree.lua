92b45b04168433e7f0f84946cd41c80b3aacbd76
leaf48 <watavita2004@gmail.com>
2023-08-10 23:48:01 -0400

    Updated: Set width in nvim-tree

--- lua/core/plugin_config/ui/nvim-tree.lua
+++ lua/core/plugin_config/ui/nvim-tree.lua
@@ -5,5 +5,12 @@ vim.g.nvim_tree_auto_open = 1
 
 require("nvim-tree").setup()
 
+require("nvim-tree").setup{
+  view = {
+    width = 40
+  }
+}
+
+
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>', {noremap = true, silent = true})
 vim.keymap.set('n', '<c-m>', ':NvimTreeFocus<CR>', {noremap = true, silent = true})
